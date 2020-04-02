923f72c5b27850430e8a28cb510f9ee8eeaefc45
uriyahr <uriyahruiz1@gmail.com>
2020-04-02 02:37:36 -0700

    Edit - App Navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,12 +2,18 @@
   <v-app id="inspire">
     <v-card color="grey lighten-4" flat height="6px" tile>
       <v-toolbar class="elevation-0">
-        <v-btn text :ripple="false" id="nav-title" class="font-weight-regular" :to="'/'"> Allergy </v-btn>
+        <!-- <v-btn text :ripple="false" id="nav-title" class="font-weight-regular" :to="'/'"> Allergy </v-btn> -->
+        <button id="nav-title" @click="route('/')" :class="{ activeButton: onAllergy }"> Allergy </button>
         <v-spacer></v-spacer>
         <div id="rightNav">
-          <v-btn shaped text small :ripple="false" id="nav-link" class="font-weight-regular " :to="'/search'"> Search </v-btn>
-          <v-btn shaped text small :ripple="false" id="nav-link" class="font-weight-regular" :to="'/signin'"> Sign In </v-btn>
-          <v-btn shaped text small :ripple="false" id="nav-link" class="font-weight-regular" :to="'/about'"> About </v-btn>
+          <!-- <button :class="{activeButton : onSearch}"> Search</button> -->
+          <button @click="route('/search')" :class="{ activeButton : onSearch }">  Search </button>
+          <button @click="route('/signin')" :class="{ activeButton : onSignIn }"> Sign In</button>
+          <button @click="route('/about')" :class="{ activeButton : onAbout }"> About</button>
+
+          <!-- <v-btn text small :ripple="false" id="nav-link" class="font-weight-regular" :to="'/search'"> Search </v-btn>
+          <v-btn text small :ripple="false" id="nav-link" class="font-weight-regular" :to="'/signin'"> Sign In </v-btn>
+          <v-btn text small :ripple="false" id="nav-link" class="font-weight-regular" :to="'/about'"> About </v-btn> -->
         </div>
       </v-toolbar>
     </v-card>
@@ -24,15 +30,18 @@ export default {
 
   }),
   methods: {
-    showLoginModal() {
-      this.$store.state.showingLogin = true;
-    }
+    route(path) {
+      if (this.$route.path != path) {
+        this.$router.push(path);
+      }
+    },
   },
   computed: {
-    showLogin() {
-      console.log(this.$store.state.showingLogin);
-      return this.$store.state.showingLogin;
-    }
+    onAllergy() { return this.$route.path == '/'; },
+    onSearch() { return this.$route.path == '/search'; },
+    onSignIn() { return this.$route.path == '/signin'; },
+    onAbout() { return this.$route.path == '/about'; }
+
   }
 };
 </script>
@@ -43,30 +52,40 @@ export default {
 #inspire{
   background-color: #d4f1e8;
 }
+.nav-toolbar {
+  background-color: white;
+}
 #nav-title {
   background-color: white !important;
-  text-transform: none !important;
-  font-family: 'Playfair Display', serif;
-  letter-spacing: 5px;
-  margin-left: 110px;
-  font-size: 16px;
   color: black;
-}
-
-#nav-link{
+  font-size: 16px;
+  letter-spacing: 5px;
+  font-family: 'Playfair Display', serif;
   text-transform: none !important;
-  font-family: 'Montserrat', sans-serif;
-  font-size: 11px;
-  letter-spacing: 1px;
-  margin-right: 5px;
+  margin-left: 110px;
+  border-radius: 0px;
+  border-bottom: none;
+}
 
-}
-/* .v-btn:focus #nav-link{
-  background-color: teal;
-}
 #rightNav {
   margin-right: 100px;
-} */
+}
 
+button {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 13px;
+  letter-spacing: 1px;
+  border-bottom: 2px solid rgba(0, 0, 0, 0);
+  color: rgb(2, 2, 2);
+  margin: 9px;
+}
 
+button.activeButton{
+  border-bottom: 2px solid teal;
+  color: teal;
+
+}
+button:focus {
+  outline: 0;
+}
 </style>
